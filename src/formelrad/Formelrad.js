62876acd5824f5e74d3b3360ad10e6fe1ee1e51a
fliberto <fabio.liberto@ergon.ch>
2024-05-28 12:03:37 +0200

    A user instruction is required

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -85,6 +85,7 @@ export default function Formelrad() {
                 <header>
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
+                    <p>Zwei Werte eingeben, die anderen werden berechnet.</p>
                 </header>
                 <form onSubmit={calculate}>
                     <InputField color={colors.u} value={values.u} label="Spannung" handleChange={e => {
